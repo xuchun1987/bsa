16cba36d75ba695bf12939a31b53c43a6060ba04
xuchun1987 <xuchun1987@yeah.net>
2017-09-28 11:04:55 +0800

    11

---
+++ src/main/java/com/bsa/controller/LoginController.java
@@ -0,0 +1,27 @@
+package com.bsa.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by xuchun on 2017/9/26.
+ */
+@Controller
+public class LoginController {
+
+
+
+
+   /* @RequestMapping("/login")
+    public String login(Model model){
+        model.addAttribute("key","hello world");
+        return "login";
+    }
+
+
+    @RequestMapping("/home")
+    public String home(Model model){
+        model.addAttribute("key","hello world");
+        return "home";
+    }
+*/
+}
